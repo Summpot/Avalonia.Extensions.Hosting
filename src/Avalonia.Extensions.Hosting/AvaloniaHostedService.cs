f2d5f3ce6f78f46445014cae987cd5ac9da958f7
Summpot <summpot@outlook.com>
2024-12-27 09:22:04 +0800

    shutdown application when host is shutting down

--- src/Avalonia.Extensions.Hosting/AvaloniaHostedService.cs
+++ src/Avalonia.Extensions.Hosting/AvaloniaHostedService.cs
@@ -9,41 +9,44 @@ using Microsoft.Extensions.Hosting;
 namespace Avalonia.Extensions.Hosting;
 
 internal class AvaloniaHostedService<TApplication, TWindow> : IHostedService
-    where TApplication : Application
-    where TWindow : Window
+  where TApplication : Application
+  where TWindow : Window
 {
-    private readonly TApplication _application;
-    private readonly TWindow? _window;
-    private readonly IHostApplicationLifetime _hostApplicationLifetime;
-    private readonly AvaloniaApplicationOptions _options;
+  private readonly TApplication _application;
+  private readonly TWindow? _window;
+  private readonly IHostApplicationLifetime _hostApplicationLifetime;
+  private readonly AvaloniaApplicationOptions _options;
 
-    public AvaloniaHostedService(TApplication application, TWindow? window,
-        IHostApplicationLifetime hostApplicationLifetime, AvaloniaApplicationOptions options)
+  public AvaloniaHostedService(
+    TApplication application,
+    TWindow? window,
+    IHostApplicationLifetime hostApplicationLifetime,
+    AvaloniaApplicationOptions options)
+  {
+    _application = application;
+    _window = window;
+    _hostApplicationLifetime = hostApplicationLifetime;
+    _options = options;
+  }
+
+  public Task StartAsync(CancellationToken cancellationToken)
+  {
+    if (_application.ApplicationLifetime is ClassicDesktopStyleApplicationLifetime desktop)
     {
-        _application = application;
-        _window = window;
-        _hostApplicationLifetime = hostApplicationLifetime;
-        _options = options;
+      desktop.MainWindow = _window;
+      desktop.Start(_options.Args ?? []);
     }
 
-    public Task StartAsync(CancellationToken cancellationToken)
-    {
-        if (_application.ApplicationLifetime is ClassicDesktopStyleApplicationLifetime desktop)
-        {
-            desktop.MainWindow = _window;
-            desktop.Start(_options.Args ?? []);
-        }
+    _hostApplicationLifetime.StopApplication();
+    return Task.CompletedTask;
+  }
 
-        _hostApplicationLifetime.StopApplication();
-        return Task.CompletedTask;
-    }
-
-    public Task StopAsync(CancellationToken cancellationToken)
+  public Task StopAsync(CancellationToken cancellationToken)
+  {
+    if (_application.ApplicationLifetime is ClassicDesktopStyleApplicationLifetime desktop)
     {
-        if (_window is not null)
-        {
-            _window.Close();
-        }
-        return Task.CompletedTask;
+      desktop.Shutdown();
     }
+    return Task.CompletedTask;
+  }
 }
